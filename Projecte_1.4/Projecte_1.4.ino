d775b44faa86bf49d1505491e318bd7c31c15edc
hickok <jmcon@hotmail.es>
2015-02-27 20:13:52 +0100

    Projecte 1.4
    fet

---
+++ Projecte_1.4/Projecte_1.4.ino
@@ -0,0 +1,14 @@
+void setup()                // run once, when the sketch starts
+{
+  Serial.begin(9600);       // set up Serial library at 9600 bps
+  Serial.println("Hello world!");
+ Serial.println("Hello world!");
+Serial.println("Hello world!");
+Serial.println("Hello world!");// prints with brake line
+}
+
+void loop()   // run over and over again
+{
+		   // do nothing
+}
+
