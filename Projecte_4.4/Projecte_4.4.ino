050d5804e576cc0a6267793104aa9f381c83b7b0
hickok <jmcon@hotmail.es>
2015-02-27 20:56:14 +0100

    Projecte 4.3
    falta revisió

---
+++ Projecte_4.4/Projecte_4.4.ino
@@ -0,0 +1,40 @@
+float KgCO2m2 = 101;
+
+void setup()              // run once, when the sketch starts
+{
+  Serial.begin(9600);     // set up Serial library at 9600 bps
+
+  if ( KgCO2m2 < 3.5)
+  {
+    Serial.print("A");
+  } 
+  else if ( KgCO2m2 < 6.5)
+  {
+    Serial.print("B");
+  }
+    else if ( KgCO2m2 < 11.1)
+  {
+    Serial.print("C");
+  }
+   else if ( KgCO2m2 < 17.7)
+  {
+    Serial.print("D");
+  }
+    else if ( KgCO2m2 < 38.2)
+  {
+    Serial.print("E");
+  }
+   else if ( KgCO2m2 < 43.2)
+  {
+    Serial.print("F");
+  } 
+   else 
+  {
+    Serial.print("G");
+  }
+}
+
+void loop()   // we need this to be here even though its empty
+{
+}
+
