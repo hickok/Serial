9535b4db21329c06bebcc0716a26179211ea42c7
hickok <jmcon@hotmail.es>
2015-03-21 11:15:27 +0100

    Projecte 1.4
    fet

--- Projecte_1.4/Projecte_1.4.ino
+++ Projecte_1.4/Projecte_1.4.ino
@@ -3,11 +3,11 @@ void setup()
   Serial.begin(9600);
   Serial.println("Escull el numero de l'operacio que vols realitzar?");
   Serial.println("1. Comprovar numero de tarjeta de credit");
-  delay(1000);
+  delay(5000);
   Serial.println("2. Comprovar numero de compte bancari");
-  delay(1000);
+  delay(5000);
   Serial.println("3. Buscar un digit perdut de tarjeta de credit");
-  delay(1000);
+  delay(5000);
 }
 
 void loop()
